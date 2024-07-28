784785962f09b50f140ab00838bf2423c52893ee
Jonas Gabriel <jonas.rusiana0@gmail.com>
2024-07-28 16:03:33 +0800

    Added Next Auth

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,45 @@
+import type { NextAuthOptions, User } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { db } from "@/lib/db";
+
+export const options: NextAuthOptions = {
+    providers: [
+        Credentials({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username",
+                    type: "text",
+                    placeholder: "Username",
+                },
+                password: {
+                    label: "Password",
+                    type: "password",
+                    placeholder: "Password",
+                },
+            },
+            async authorize(credentials) {
+                const username = credentials?.username as string;
+                const password = credentials?.password as string;
+                const user = await db.user.findUnique({
+                    where: {
+                        username: username,
+                    },
+                });
+
+                if (user?.password === password) {
+                    const result: User = {
+                        id: user.user_id.toString(),
+                        name: user.username,
+                    }
+                    return result;
+                } else {
+                    return null;
+                }
+            },
+        })
+    ],
+    pages: {
+        signIn: '/login',
+    }
+}
